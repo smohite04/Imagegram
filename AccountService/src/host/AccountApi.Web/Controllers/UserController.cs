8e3ffdc09fd2d27b5fed4b5766df53b0e2cd2de7
tavisca-smohite <smohite@TAVLAPT203>
2021-05-10 15:31:34 +0530

    added the stub for account service

---
+++ AccountService/src/host/AccountApi.Web/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AccountApi.Web.Controllers
+{
+    [Route("api/v1.0/[controller]")]
+    public class UserController : Controller
+    {
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        [TypeFilter(typeof(UserAuthorizationFilter))]
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AccountService/src/host/AccountApi.Web/Filters/UserAuthorizationFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountApi.Web
+{
+    public class UserAuthorizationFilter : IAuthorizationFilter
+    {
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            //TODO : add token based authorization based on value in header
+        }
+    }
+}
